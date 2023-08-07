e1b1aa608abbf479819d301c93ea0d3c01593bf3
pastila <vajla@mail.ru>
2023-08-07 13:18:57 +0400

    исправлено написание формулы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,8 +7,8 @@ function solveEquation(a, b, c) {
 	if (discriminant === 0) {
 		arr.push(-b / (2 * a));
 	} else if (discriminant > 0) {
-		arr.push(-b + Math.sqrt(discriminant)) / (2 * a);
-		arr.push(-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
 	}
 	return arr;
 }
@@ -19,7 +19,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	}
 	let monthlyPercent = percent / 100 / 12;
 	let principalAmount = amount - contribution;
-	let monthlyPayment = principalAmount * (monthlyPercent / (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+	let monthlyPayment = principalAmount * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
 	let totalAmount = (monthlyPayment * countMonths).toFixed(2);
 	let answer = Number(totalAmount);
 	return answer;
