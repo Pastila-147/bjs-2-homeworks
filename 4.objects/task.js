ba6f4e325a297225eb83f2fdd5f236d5bf77a59b
pastila <vajla@mail.ru>
2023-08-27 13:24:36 +0400

    исправлен вывод функции

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,31 @@
 function Student(name, gender, age) {
-  this.name = name,
-  this.gender = gender,
-  this.age = age,
-  this.marks = []
+	this.name = name,
+		this.gender = gender,
+		this.age = age,
+		this.marks = []
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  if (this.hasOwnProperty("marks")){ 
-  this.marks.push(...marks);
-}
-}
-
-Student.prototype.getAverage = function () {
-  if (this.hasOwnProperty("marks")|| this.marks.length !== 0) { 
-    const totalMarks = this.marks.reduce((acc, mark) => acc + mark, 0);
-    this.averageMarks = totalMarks / this.marks.length;
-  }
-  else {
-    this.averageMarks = 0;
-  }
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  delete this.marks;
-  delete this.subject;
-  this.excluded = reason;
+Student.prototype.getAverage = function() {
+	if (this.hasOwnProperty("marks") || this.marks.length !== 0) {
+		const totalMarks = this.marks.reduce((acc, mark) => acc + mark, 0);
+		return totalMarks / this.marks.length;
+	} else {
+		return 0;
+	}
+}
+
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
