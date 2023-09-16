6c79dddd6c4b95a96b6f9f7a52d127ee4a649454
pastila <vajla@mail.ru>
2023-09-16 17:24:02 +0400

    добавлено решение задачи

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, actionparametr) {
+		if (!time || !actionparametr) {
+			throw new Error("Отсутствуют обязательные аргументы");
+		} else if (this.alarmCollection.find(alarm => alarm.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
+
+		return this.alarmCollection.push({
+			callback,
+			time,
+			canCall: true
+		})
+
+	}
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.time !== time);
+	}
+	getCurrentFormattedTime() {
+		return new Date().toTimeString().substring(0, 5);
+	}
+
+	start() {
+		if (this.intervalId != null) {
+			return
+		}
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach((alarm) => {
+				if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall === true) {
+					alarm.canCall = false;
+					alarm.callback();
+				}
+			})
+		}, 1000);
+	}
+
+	stop() {
+		clearInterval(intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach((alarm) =>
+			alarm.canCall = true)
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
